fcac8c9c8743c673a7314b1b9e7ded5884c30aca
Eman <emanwel2@gmail.com>
2019-11-11 16:14:08 +0300

    Added codes that runs moses on multiple datasets

--- scripts/benchmark/asmoses-bm.sh
+++ scripts/benchmark/asmoses-bm.sh
@@ -1,28 +1,28 @@
-# Calls asmoses a number of times and reports the mean and std time of
-# its runs
+## Calls asmoses a number of times and reports the mean and std time of
+## its runs
 
-# This script relies on https://github.com/nferraz/st
-
-# Check unbound variables
+## This script relies on https://github.com/nferraz/st
+#
+## Check unbound variables
 set -u
-
-# # Debug trace
-# set -x
-
-#############
-# Functions #
-#############
-
-# Get the real time in second (given the output of time command)
+#
+## # Debug trace
+## set -x
+#
+##############
+## Functions #
+##############
+#
+## Get the real time in second (given the output of time command)
 get_real_time() {
     grep "real" | cut -d' ' -f2
 }
-
-########
-# Main #
-########
-
-# Parse comment argument
+#
+#########
+## Main #
+#########
+#
+## Parse comment argument
 if [ $# == 0 ]; then
     echo "Wrong number of arguments."
     echo
@@ -33,18 +33,22 @@ if [ $# == 0 ]; then
     echo "and return statistics about its execution time."
     exit 1
 else
-    REPEAT=$1
+    REPEAT=$2
+    DIR=$1
 fi
-
-# Discard REPEAT argument
-shift
-
-# Run asmoses REPEAT times
+#
+## Discard REPEAT argument
+shift 2
+#echo $REPEAT
+#
+## Run asmoses REPEAT times
 for i in $(seq 1 $REPEAT); do
     if [[ "$i" != "$REPEAT" ]]; then
         echo -en "\rRun asmoses $@ ($i/$REPEAT)" 1>&2
     else
-        echo -e "\rRun asmoses $@ ($i/$REPEAT)" 1>&2
+        echo -e "\rRun asmoses $@ ($i/$REPEAT)"  1>&2  >> $DIR/asmoses-bench.log 
+	#echo  -en "\t" >>$DIR/asmoses-bench.log
     fi
     (time -p asmoses $@ 1>/dev/null) 2>&1
-done | get_real_time | st | column -t
+done  | get_real_time | st  >> $DIR/asmoses-bench.log
+
--- scripts/benchmark/mb-example.sh
+++ scripts/benchmark/mb-example.sh
@@ -1,12 +1,11 @@
 # Example program using asmoses-bm.sh to benchmark a series of runs
 # with some given options
-
 #############
 # Constants #
 #############
 
 # Run of times to repeat each run (as to gather statistics)
-REPEAT=10
+REPEAT=3
 
 # Get the script directory
 PRG_PATH="$(readlink -f "$0")"
@@ -15,11 +14,33 @@ PRG_DIR="$(dirname "$PRG_PATH")"
 ########
 # Main #
 ########
+echo "(INFO $(whoami) $(date) )" >> $PRG_DIR/asmoses-bench.log
+echo "Run asmoses $REPEAT times on multiple problems of various complexity" >> $PRG_DIR/asmoses-bench.log
+echo " " >> $PRG_DIR/asmoses-bench.log
+echo "---------------------------------------------------------------------" >> $PRG_DIR/asmoses-bench.log
+echo " " >> $PRG_DIR/asmoses-bench.log
 
-echo "Run asmoses $REPEAT times on multiple problems of various complexity"
-
-for pbl in pa dj mux maj sr; do
+for pbl in pa  mux; do
     for k in {3..5}; do
-        $PRG_DIR/asmoses-bm.sh $REPEAT -H$pbl -k$k -m10000
+         $PRG_DIR/asmoses-bm.sh $PRG_DIR  $REPEAT -H$pbl -k$k -m2000 -p0.0001
+	 $PRG_DIR/asmoses-bm.sh $PRG_DIR  $REPEAT -H$pbl -k$k -m2000 -p0.0001 --atomspace-port=1
+	$PRG_DIR/asmoses-bm.sh $PRG_DIR  $REPEAT -H$pbl -k$k -m2000 -p0.0001 --atomspace-port=1 --atomspace-store=1
     done
 done
+
+
+
+echo "Run asmoses $REPEAT times on Melanoma and Cowles dataset " >> $PRG_DIR/asmoses-bench.log
+echo " " >> $PRG_DIR/asmoses-bench.log                                                                   
+echo "---------------------------------------------------------------------" >> $PRG_DIR/asmoses-bench.log 
+echo " " >> $PRG_DIR/asmoses-bench.log
+for file in datasets/*
+do	
+	$PRG_DIR/asmoses-bm.sh $PRG_DIR $REPEAT -i $file -m10000
+	$PRG_DIR/asmoses-bm.sh $PRG_DIR $REPEAT -i $file -m10000 --atomspace-port=1
+	$PRG_DIR/asmoses-bm.sh $PRG_DIR $REPEAT -i $file -m10000 --atomspace-port=1 --atomspace-store=1
+
+done
+
+
+
